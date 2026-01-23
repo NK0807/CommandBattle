0092a33b0bebcadb0bf58d25d51e559cfb892760
NK0807 <hyue3434@gmail.com>
2026-01-23 02:12:39 +0000

    attackメソッドを抽象化し、各サブクラスに実装

--- src/Enemy.java
+++ src/Enemy.java
@@ -4,4 +4,33 @@ public class Enemy extends Character{
 	public Enemy(String name, int hp, int attack) {
 		super(name, hp, attack);
 	}
+
+	// 攻撃
+	@Override
+    public void attack(Character target){
+        // ダメージが-3～3ぶれる
+        int randDamage = rand.nextInt(7) - 3;
+        int finalDamage = this.attack + randDamage;
+
+        // ダメージがマイナスにならないようにする
+        if(finalDamage < 0){
+            finalDamage = 0;
+        }
+
+        // クリティカル判定
+        int randCritical = rand.nextInt(10);
+        if(randCritical == 0){
+            // 二倍ダメージ
+            finalDamage *= 2;
+        }
+
+        // ダメージを与える
+        target.damage(finalDamage);
+
+        System.out.println(this.name + "の攻撃");
+        if(randCritical == 0){
+            System.out.println("会心の一撃！！！");
+        }
+        System.out.println(target.name + "に" + finalDamage + "のダメージ");
+    }	
 }
